28acc4a283282ceeb75fcf9ea51463866e58f147
Alex7694 <ztoropov@gmail.com>
2020-06-09 09:26:47 +0300

    Task_17

---
+++ src/Java_For_Beginners/Task_17.java
@@ -0,0 +1,24 @@
+package Java_For_Beginners;
+
+import java.io.*;
+
+public class Task_17 {
+    public static void main(String[] args) {
+        String str;
+        BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Признак конца - стоп");
+        try (FileWriter fw = new FileWriter("E:\\ rtx.txt", true)) {
+            do {
+                System.out.println("Введи текст для добавления в файл. Нажми stop для остановки: ");
+                str = br.readLine();
+                if(str.compareTo("stop") == 0) break;
+                str = str + "\n";
+                fw.write(str);
+            } while (str.compareTo("stop") != 0);
+        }catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }catch (IOException e) {
+            System.out.println("Ошибка при работе с файлом");
+        }
+    }
+}
