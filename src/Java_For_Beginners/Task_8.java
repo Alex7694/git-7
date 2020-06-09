fbdb10a0b89a118fd64a06972dbe586330255b63
Alex7694 <ztoropov@gmail.com>
2020-06-09 09:14:22 +0300

    Task_8

---
+++ src/Java_For_Beginners/Task_8.java
@@ -0,0 +1,12 @@
+package Java_For_Beginners;
+
+public class Task_8 {
+
+    public static void main(String[] args) {
+        int c = 1;
+        while (c <= 50) {
+            System.out.println(c);
+            c++;
+        }
+    }
+}
