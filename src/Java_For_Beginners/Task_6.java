7f6ecd39b5352dbd9da8ec16d6290a3df0fbef7c
Alex7694 <ztoropov@gmail.com>
2020-06-14 12:26:48 +0300

    Task_6.java

--- src/Java_For_Beginners/Task_6.java
+++ src/Java_For_Beginners/Task_6.java
@@ -6,24 +6,24 @@ public class Task_6 {
     public static void main(String[] args) {
         try {
             Scanner in = new Scanner(System.in);
-            System.out.print("Введите первое целое число: ");
-            int input_number1 = in.nextInt();
-            System.out.print("Введите второе целое число: ");
-            int input_number2 = in.nextInt();
-            System.out.print("Введите третье целое число: ");
-            int input_number3 = in.nextInt();
-            int middle = (input_number1 + input_number2 + input_number3)/3;
+            System.out.print("Введите первое число: ");
+            double input_number1 = in.nextDouble();
+            System.out.print("Введите второе число: ");
+            double input_number2 = in.nextDouble();
+            System.out.print("Введите третье число: ");
+            double input_number3 = in.nextDouble();
+            double middle = (input_number1 + input_number2 + input_number3)/3;
 
             System.out.println("Cреднее арифметическое чисел x, y, z равно: " + middle);
 
-            int cc = middle/2;
+            int  cc = (int)(middle/2);
             System.out.println("Деление среднего арифметического на 2 без остатка равно: " + cc);
 
             if (cc > 3)
                 System.out.println("Программа выполнена корректно");
             in.close();
         }catch (InputMismatchException e) {
-            System.out.println("Вы ввели не целое число");
+            System.out.println("Вы ввели не число");
         }
     }
 }
