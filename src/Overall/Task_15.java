696c2622f8fba0689a216c44d7d77457674913a8
Alex7694 <ztoropov@gmail.com>
2020-06-14 12:16:55 +0300

    Task_19

---
+++ src/Overall/Task_15.java
@@ -0,0 +1,11 @@
+//import java.io.File;
+//import java.io.FileWriter;
+//
+//public class Task_15 {
+//    public static void main(String[] args) {
+//        File file = new File("\"E:\\\\ rtx.txt\"");
+//        file.createNewFile();
+//        FileWriter writer = new FileWriter(file);
+//        writer.write();
+//    }
+//}
---
+++ src/Overall/Task_19.java
@@ -0,0 +1,8 @@
+package Overall;
+
+public class Task_19 {
+    public static void main(String[] args) {
+double[] myar = {2.4,3.8,11.2,9.8,1.18};
+        System.out.println(myar.length);
+    }
+}
