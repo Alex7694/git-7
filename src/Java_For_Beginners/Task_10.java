cbab5cb2f6f03189500bc589ad9404e0139e74d1
Alex7694 <ztoropov@gmail.com>
2020-06-21 07:26:32 +0300

    Task_10

--- src/Java_For_Beginners/Task_10.java
+++ src/Java_For_Beginners/Task_10.java
@@ -3,34 +3,27 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Task_10 {
-    public static void main(String[] args) {
-        try {
-            int a, b;
-            Scanner in = new Scanner(System.in);
-            System.out.print("Введите количество строк массива: ");
-            a = in.nextInt();
-            System.out.print("Введите количество столбцов массива: ");
-            b = in.nextInt();
-            int[][] arr = new int[a][b];
-            for (int i = 0; i < arr.length; i++) {
-                for (int j = 0; j < arr[i].length; j++) {
-                    System.out.print("Введите элемент массива [" + i + "][" + j + "]: ");
-                    arr[i][j] = in.nextInt();
+        public static void main(String[] args) {
+            try {
+                Scanner in = new Scanner(System.in);
+                System.out.print("Введите количество строк массива: ");
+                int a = in.nextInt();
+                System.out.print("Введите количество столбцов массива: ");
+                int b = in.nextInt();
+                int[][] arr = new int[a][b];
+                for (int i = 0; i < arr.length; i++) {
+                    for (int j = 0; j < arr[i].length; j++) {
+                        System.out.print("Введите элемент массива [" + i + "][" + j + "]: ");
+                        arr[i][j] = in.nextInt();
+                    }
                 }
-            }
-            in.close();
-            System.out.print("Первая строка матрицы, умноженная на 3: ");
-            System.out.print("[");
-            for (int i = 0; i < 1; i++) {
-                for (int j = 0; j < arr[i].length; j++) {
-                    System.out.print(" " + arr[i][j] * 3 + " ");
+                in.close();
+                System.out.print("Первая строка матрицы, умноженная на 3: ");
+                for (int j = 0; j < arr[0].length; j++) {
+                    System.out.print(" " + arr[0][j] * 3 + " ");
                 }
-                System.out.print("]");
+            }catch (InputMismatchException e) {
+                System.out.println("Вы ввели не целое число");
             }
-            in.close();
-        }catch (InputMismatchException e) {
-            System.out.println("Вы ввели не целое число");
         }
-
     }
-}
